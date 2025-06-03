82472b2abd3372b2545ecca42c66d6358953d554
Rémi <59444214+remi-h@users.noreply.github.com>
2025-06-03 23:12:43 +0200

    Update LastUpdatedBadge.tsx

--- components/LastUpdatedBadge.tsx
+++ components/LastUpdatedBadge.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Badge } from "@radix-ui/themes";
 
 const LastUpdatedBadge: React.FC = () => {
-  return <Badge color="blue">Updated: 03/2025</Badge>;
+  return <Badge color="blue">Updated: 06/2025</Badge>;
 };
 
 export default LastUpdatedBadge;
